4615676e1ac3f6c9ce6c6f832175ac5a3495097f
418181 <115142558+418181@users.noreply.github.com>
2023-04-13 16:38:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,69 @@
-let position = 0
-input.onGesture(Gesture.Shake, function () {
-    if (position < 10 || position > 350) {
-        basic.showNumber(1)
-    } else if (position < 100 && position > 80) {
-        basic.showNumber(2)
-    } else if (position < 190 && position > 170) {
-        basic.showNumber(3)
-    } else if (position < 280 && position > 260) {
-        basic.showNumber(4)
-    } else {
-        basic.showNumber(randint(5, 6))
+input.onButtonPressed(Button.A, function () {
+    if (a == 1) {
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Forever)
+    } else if (a == 2) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
+    } else if (a == 3) {
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Forever)
+    } else if (a == 4) {
+        music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Forever)
+    } else if (a == 5) {
+        music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Forever)
+    } else if (a == 6) {
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Forever)
+    } else if (a == 7) {
+        music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Forever)
+    } else if (a == 8) {
+        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Forever)
+    } else if (a == 9) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
+    } else if (a == 10) {
+        music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Forever)
+    } else if (a == 11) {
+        music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Forever)
+    } else if (a == 12) {
+        music.startMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Forever)
+    } else if (a == 13) {
+        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Forever)
+    } else if (a == 14) {
+        music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Forever)
+    } else if (a == 15) {
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Forever)
+    } else if (a == 16) {
+        music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Forever)
+    } else if (a == 17) {
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Forever)
+    } else if (a == 18) {
+        music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Forever)
+    } else if (a == 19) {
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Forever)
+    } else if (a == 20) {
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Forever)
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    step += 1
+    basic.showNumber(step)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + 25 * step + "cm")
+})
+input.onButtonPressed(Button.B, function () {
+    a += 1
+})
+let a = 0
+let step = 0
+step = 0
+a = 0
 basic.forever(function () {
-    position = input.compassHeading()
+    if (step == 100) {
+        basic.showIcon(IconNames.Heart)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+    } else if (step == 1000) {
+        basic.showIcon(IconNames.Surprised)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+    } else if (step == 5000) {
+        basic.showIcon(IconNames.Happy)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
+    }
 })
